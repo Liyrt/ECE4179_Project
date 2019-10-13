4d10cfd7cb19b5cd5e29563ded17ab72a6523f4c
Richard Yan <richardliyrt@live.com.au>
2019-10-14 00:04:37 +1100

    Added FID score computation, and started latent vector backpropagation code to match a target image

--- gan.py
+++ gan.py
@@ -9,10 +9,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import multiprocessing
 import numpy as np
+from scipy import linalg
 import matplotlib.pyplot as plt
 import matplotlib.colors as mpcol
 import copy
 import os
+from inception import InceptionV3
 
 class Generator(nn.Module):
 
@@ -162,7 +164,228 @@ def plotLosses(gen_losses, disc_losses, batch_per_ep):
     plt.tight_layout()
 
 
+
+
+def get_activations(images, model, batch_size=50, dims=2048,
+                    cuda=False, verbose=False):
+    model.eval()
+
+    if images.shape[0] % batch_size != 0:
+        print(('Warning: number of images is not a multiple of the '
+               'batch size. Some samples are going to be ignored.'))
+    if batch_size > images.shape[0]:
+        print(('Warning: batch size is bigger than the data size. '
+               'Setting batch size to data size'))
+        batch_size = images.shape[0]
+
+    n_batches = images.shape[0] // batch_size
+    n_used_imgs = n_batches * batch_size
+
+    pred_arr = np.empty((n_used_imgs, dims))
+
+    for i in range(n_batches):
+        if verbose:
+            print('\rPropagating batch %d/%d' % (i + 1, n_batches),
+                  end='', flush=True)
+        start = i * batch_size
+        end = start + batch_size
+
+        torch_batch = images[start:end,:,:,:]
+
+        # Reshape to (n_images, 3, height, width)
+        # image_batch = image_batch.transpose((0, 3, 1, 2))
+        # image_batch /= 255
+        #
+        # torch_batch = torch.from_numpy(image_batch).type(torch.FloatTensor)
+        if cuda:
+            torch_batch = torch_batch.cuda()
+        with torch.no_grad():
+            pred = model(torch_batch)[0]
+
+        # If model output is not scalar, apply global spatial average pooling.
+        # This happens if you choose a dimensionality not equal 2048.
+        if pred.shape[2] != 1 or pred.shape[3] != 1:
+            pred = F.adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+        pred_arr[start:end] = pred.cpu().data.numpy().reshape(batch_size, -1)
+
+    if verbose:
+        print(' done')
+
+    return pred_arr
+
+
+def calculate_activation_statistics(num_images, generator, inceptionModel, batch_size=50,
+                                    dims=2048, cuda=False, verbose=False):
+    # NEEDED, otherwise not enough CUDA memory available due to intermediate variables not being freed (required for grad)
+    # (f(g(x))' = f'(g(x))*g'(x)
+    with torch.no_grad():
+        n_batches = num_images // batch_size
+        act = np.zeros((n_batches*batch_size, dims))
+        # Do multiple image sub batches here. Save memory
+        for i in range(n_batches):
+            start = i * batch_size
+            end = start + batch_size
+            latent_noise = torch.randn(batch_size, noise_dim, 1, 1, device=device)
+            gen_images = generator(latent_noise)
+            # Normalise to [0,1] instead of [-1,1]. Inception then normalises again to [-1,1]
+            gen_images += 1
+            gen_images *= 0.5  # In-place python operations save GPU memory
+
+            act[start:end] = get_activations(gen_images, inceptionModel, batch_size, dims, cuda, verbose)
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    https://github.com/mseitzer/pytorch-fid
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    Stable version by Dougal J. Sutherland.
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+    Returns:
+    --   : The Frechet Distance.
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) +
+            np.trace(sigma2) - 2 * tr_covmean)
+
+
+def figGenVsCelebA(generator, inceptionModel, num_images=10000, batch_size=100, cuda=True):
+    mu_gen, sigma_gen = calculate_activation_statistics(num_images, generator, inceptionModel, batch_size=batch_size,
+                                        dims=2048, cuda=cuda, verbose=False)
+
+    celeba_precalc = np.load('fid_stats_celeba.npz')
+    celeba_precalc_mu = celeba_precalc.f.mu
+    celeba_precalc_sigma = celeba_precalc.f.sigma
+    celeba_precalc.close()
+
+    fid_value = calculate_frechet_distance(mu_gen, sigma_gen, celeba_precalc_mu, celeba_precalc_sigma, eps=1e-6)
+    return fid_value
+
+
+def fid_stats_data(dataset, inceptionModel, filename):
+    batch_size = 100
+    full_loader_fid = datautils.DataLoader(dataset, batch_size=batch_size,
+                                             shuffle=False, num_workers=n_workers, drop_last=True)
+
+    act = np.zeros((batch_size*len(full_loader_fid), 2048))
+
+    with torch.no_grad():
+        for i, (images, samples) in enumerate(full_loader_fid):
+            images += 1
+            images *= 0.5  # In-place python operations save GPU memory
+
+            act[i*batch_size:i*batch_size+batch_size] = get_activations(images, inceptionModel, batch_size, 2048, cuda=cuda)
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+
+    np.savez(filename, mu=mu, sigma=sigma)
+    return mu, sigma
+
+
+def reconImage(obs_images, boxes, generator, discriminator):
+    # ONLY WORKS WITH ONE IMAGE
+    num_steps = 2000
+    obs_images = obs_images.to(device)
+    outobsvis = torchvision.utils.make_grid(obs_images.detach().cpu(), normalize=True)
+    plt.imshow(outobsvis.numpy().transpose((1, 2, 0)))
+    num_images = obs_images.shape[0]
+    noise_dim = 100
+    BCE_R = nn.BCELoss()
+    MSE_R = nn.MSELoss()
+    label_real = torch.full((num_images,), 1, device=device)
+
+
+    # Need to ensure that requires_grad is set to True after the variable is created on the desired device
+    latent_noise = torch.randn(num_images, noise_dim, 1, 1, device=device, requires_grad=True)
+    lr = 0.1
+    reconOptim = optim.SGD([latent_noise], lr=lr)
+
+    recon_loss_scale = 0
+
+    gen_progress = []
+    losses = []
+    for step in range(num_steps):
+        ### THEN TRY WITH ADAM
+        gen_images = generator(latent_noise)
+        if step % 50 == 0:
+            gen_progress.append(gen_images.detach().cpu())
+        D_gen_out = D(gen_images)
+        D_gen_loss = BCE_R(D_gen_out, label_real)
+
+        # Let's try MSE LOSS ONLY IN CENTRAL REGION
+        recon_loss = MSE_R(obs_images[:,:,16:49,16:49], gen_images[:,:,16:49,16:49])        # Make this a function of corrupted box
+        # Find that we need to scale the reconstruction penalty much more
+        # We may even want to make the recon_loss scale a function of the number of steps. We would want to initially
+        # have a highly penalised reconstruction loss to get close to the image we want to reconstruct.
+        # Then we we are closer we wish to make that image more realistic
+
+        total_loss = D_gen_loss + recon_loss_scale*recon_loss
+        losses.append(total_loss.item())
+
+        reconOptim.zero_grad()
+        total_loss.backward()
+        reconOptim.step()
+
+        # USING MANUAL UPDATE (NO OPTIMISER)
+        # gradient = latent_noise.grad.detach()
+        # latent_noise.data -= lr*gradient
+        #
+        # latent_noise.grad.zero_()
+
+    gen_progress = torch.cat(gen_progress, dim=0)
+    plt.figure(figsize=(12,8))
+    outvis = torchvision.utils.make_grid(gen_progress.detach().cpu(), normalize=True)
+    plt.imshow(outvis.numpy().transpose((1, 2, 0)))
+
 if __name__ == "__main__":
+
+    ### TESTING WITH REPRODUCIBILITY
+    torch.manual_seed(0)
+    np.random.seed(0)
+
     Model_Path = "Models"
     if not os.path.isdir(Model_Path):
         os.makedirs(Model_Path)
@@ -170,19 +393,20 @@ if __name__ == "__main__":
     cuda = torch.cuda.is_available()
     device = torch.device("cuda" if cuda else "cpu")
     start_from_checkpoint = True
-
     batch_size = 500
     n_workers = 4
+
+    crop_image = True
     image_size = 64
     dataset = torchvision.datasets.ImageFolder(root="celeba",
                                                transform=transforms.Compose([
-                                                   transforms.Resize(image_size),
-                                                   transforms.CenterCrop(image_size),
+                                                   #transforms.Lambda(lambda img: transforms.functional.crop(img, ))
+                                                   transforms.CenterCrop(144 if crop_image else image_size),
+                                                   transforms.Resize(64),
                                                    transforms.ToTensor(),
-                                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # Into [-1,1]
                                                ]))
-    # To actually crop, remove resize, or change its argument
-
+    # Apply black box obstruction transform using a lambda function probably
     #trainsamp = datautils.SubsetRandomSampler(fixedsplit_train)    # Doesn't work with shuffle
     # If we want a fixed split use: datautils.Subset(dataset, indices), and initialise a dataloader with this
     num_train = round(0.95*len(dataset))
@@ -195,6 +419,16 @@ if __name__ == "__main__":
     testloader = datautils.DataLoader(testdata, batch_size=batch_size,
                                              shuffle=False, num_workers=n_workers)
 
+    # inceptionModel = InceptionV3().to(device)
+
+
+    # real_batch = next(iter(testloader))
+    # plt.figure(figsize=(8, 8))
+    # plt.axis("off")
+    # plt.title("Training Images")
+    # plt.imshow(np.transpose(torchvision.utils.make_grid(real_batch[0].to(device)[:16], padding=2, normalize=True).cpu(), (1, 2, 0)))
+
+
     noise_dim = 100
     G = Generator(64).to(device)
     G.apply(weight_init)
@@ -202,12 +436,6 @@ if __name__ == "__main__":
     D = Discriminator(64).to(device)
     D.apply(weight_init)
 
-    # real_batch = next(iter(testloader))
-    # plt.figure(figsize=(8, 8))
-    # plt.axis("off")
-    # plt.title("Training Images")
-    # plt.imshow(
-    #     np.transpose(torchvision.utils.make_grid(real_batch[0].to(device)[:16], padding=2, normalize=True).cpu(), (1, 2, 0)))
 
     lr = 0.0002
     num_epochs = 10
@@ -221,14 +449,18 @@ if __name__ == "__main__":
     fixed_latent_noise = torch.randn(8, noise_dim, 1, 1).to(device)
     test_images_log = []
 
-    model_name = "FirstModel"
+    model_name = "FirstModelCropped144"
     Save_Path = os.path.join(Model_Path, model_name + ".pt")
     start_epoch = 0
     if start_from_checkpoint:
         start_epoch, G_losses, D_losses = loadCheckpoint(G, G_optimiser, D, D_optimiser, model_name)
-    latent_noise = torch.randn(8, noise_dim, 1, 1, device=device)
-    test_images_log = [G(latent_noise)]
-    visualiseProgress(test_images_log)
+    reconImage(dataset[0][0].unsqueeze(dim=0), [], G, D)
+    # Apply basic Frechet distance calculation
+
+    # fid_value = figGenVsCelebA(G, inceptionModel, num_images=10000, batch_size=100, cuda=cuda)
+
+    #test_images_log = [G(fixed_latent_noise)]
+    #visualiseProgress(test_images_log)
     for epoch in range(start_epoch, num_epochs):
 
         for it, data in enumerate(trainloader):
@@ -275,7 +507,7 @@ if __name__ == "__main__":
         test_fake = G(fixed_latent_noise)
         test_images_log.append(test_fake.detach())
 
-        visualiseProgress(test_images_log, "Fake_Images_Progress"+str(epoch+1))
+        visualiseProgress(test_images_log, "Fake_Images_Progress_Cropped"+str(epoch+1))
         # Maintain the most recent model state (weights) (not necessarily the best training loss)
         # COPY TO DISK
         torch.save({
